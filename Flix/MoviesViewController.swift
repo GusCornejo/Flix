b84c6fe77a90f0e0a5f7517637c65f14e6abaac3
GusCornejo <gustavoxcornejo@knights.ucf.edu>
2021-09-19 12:07:46 -0400

    Required Code Finished

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -66,14 +66,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
         // Get the new view controller using segue.destination.
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
         // Pass the selected object to the new view controller.
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
