2235a133f40192227f94bf89920380a7b710c8c9
GusCornejo <gustavoxcornejo@knights.ucf.edu>
2021-09-20 13:02:54 -0400

    README updated - gif added

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -65,4 +65,17 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.item]
+        
+        let detailsViewController = segue.destination as! SuperheroDetailsViewController
+        detailsViewController.movie = movie
+        
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
 }
